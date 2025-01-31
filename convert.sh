0e2e446252457462f18f08dcf263c7ab01727e73
joeriddles <joeriddles10@gmail.com>
2025-01-30 22:09:43 -0700

    Move generated files to .connect/*

--- convert.sh
+++ convert.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-for file in "./generated"/*".json"; do
+for file in ".connect"/*/*".json"; do
   if [ -f "$file" ]; then
     BASE_FILE=$(echo "$file" | cut -d '.' -f -2)
-    YAML_FILE="$BASE_FILE.yaml"
+    YAML_FILE="$BASE_FILE.yml"
     cat "$file" | yq -P > "$YAML_FILE"
   fi
 done
--- main.py
+++ main.py
@@ -17,19 +17,22 @@ if __name__ == "__main__":
     # print(result.usage())
     with capture_run_messages() as messages:
         try:
-            result = agent.run_sync("Configure an RSS feed source component")
-            print(result.data)
-
-            # Save generated model to file
-            model_json = result.data.model_dump_json(
-                indent=4, exclude_none=True, exclude_unset=True
-            )
-            component_name = result.data.name.replace(" ", "_").casefold()
-            with open(f"./generated/{component_name}.json", "w") as fout:
-                fout.write(model_json)
-
+            result = agent.run_sync("Configure an RSS feed sink component")
         except UnexpectedModelBehavior:
             for message in messages:
                 json.dump(
                     dataclasses.asdict(message), sys.stdout, indent=4, default=str
                 )
+            sys.exit(1)
+
+    print(repr(result.data))
+
+    # Save generated model to file
+    model_json = result.data.model_dump_json(
+        indent=4, exclude_none=True, exclude_unset=True
+    )
+    kind = result.data.kind.value
+    component_name = result.data.name.replace(" ", "_").casefold()
+    filepath = f".connect/{kind}s/{component_name}.json"
+    with open(filepath, "w") as fout:
+        fout.write(model_json)
